fc354c56a185472f649adbbf160545089491d580
aamrlyman <aamrlyman@gmail.com>
2023-03-13 20:45:39 -0700

    reconfigure url for deployment

---
+++ src/urlHost.js
@@ -0,0 +1,8 @@
+export const URL_HOST = 
+    process.env.NODE_ENV === "production"
+        ? "https://cors-everywhere-me.herokuapp.com/http://devOpsPracticeBackend2-env.eba-aqhajsas.us-west-2.elasticbeanstalk.com"
+        : "http://127.0.0.1:8000";
+
+
+        //npm run build for production mode, npm start for development mode
+        // devOpsPracticeBackend2-env.eba-aqhajsas.us-west-2.elasticbeanstalk.com
